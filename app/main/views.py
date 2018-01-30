d05e4de5112a15825f8c23aeaf7f7548f0297053
Alwatan <pugudean@gmail.com>
2018-01-30 03:31:06 +0300

    added profile view

--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,12 @@
 from flask import session, flash,redirect,url_for,render_template
+from ..models import User
 from . import main
 
 @main.route('/')
 def index():
     return render_template('index.html')
+
+@main.route('/user/<username>')
+def user(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    return render_template('user.html', user=user)
